fde57c1e1a59fa427b0144cf492807eb19c384f5
Pur3-MAlice <aliceford98@live.co.uk>
2023-03-02 16:48:14 +0000

    test: testing array knowledge

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
-let myArray = ["hello", "is", "it", "me", 45, true, null]
+let myArray = ["hello", "is", "it", "me", 45, true, "guff"];
 
-console.log(myArray)
+console.log(myArray);
+
+myArray.pop();
+
+console.log(myArray);
+
+myArray.unshift("guff");
+
+console.log(myArray);
+
+myArray.sort();
+
+console.log(myArray);
+
+console.log(myArray[4]);
+
+let newArray = myArray.filter(function(remmey) {
+    return remmey !== 'it';
+});
+
+console.log(myArray);
+console.log(newArray);
